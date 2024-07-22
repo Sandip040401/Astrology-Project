d1af3c42073308fff3a633f681a50987fa3130b7
Shamim Imran <shamimimran35@gmail.com>
2024-07-22 14:17:11 +0530

    navbar Adeded

--- astrology-app-client/src/pages/Home.jsx
+++ astrology-app-client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import { Navbar } from "../components/Navbar";
 
+import Carousel from "../components/Carousel";
+import { ContactUs } from "../components/ContactUs";
+import { Navbar } from "../components/Navbar";
 
 export default function Home(){
     return(
         <>
             <Navbar/>
+            <Carousel/>
+            <ContactUs/>
         </>
     )
 }
